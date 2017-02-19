408a259ed10ad163974756728157983296767853
Elena Zarkova <ezarkova@gmail.com>
2017-02-20 00:21:13 +0200

    Added more user service tests

---
+++ CourseProject/CourseProject.Services.Tests/UsersServiceTests/GetUserByUsername_Should.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using Moq;
+using CourseProject.Data.UnitsOfWork;
+using CourseProject.Data.Repositories;
+using CourseProject.Models;
+
+namespace CourseProject.Services.Tests.UsersServiceTests
+{
+    [TestFixture]
+    public class GetUserByUsername_Should
+    {
+        [Test]
+        public void CallRepositoryGetAllWithFilterMethod()
+        {
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedUsersRepo = new Mock<IGenericRepository<User>>();
+
+            mockedUsersRepo.Setup(x => x.GetAll(It.IsAny<Expression<Func<User, bool>>>())).Returns(new List<User>());
+
+            var service = new UsersService(mockedUnitOfWork.Object,mockedUsersRepo.Object);
+
+            service.GetUserByUsername("the-super-cool-username-007");
+
+            mockedUsersRepo.Verify(
+                x => x.GetAll(It.IsAny<Expression<Func<User, bool>>>()), 
+                Times.Once);
+        }
+
+        [Test]
+        public void ReturnTheFounUser()
+        {
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedUsersRepo = new Mock<IGenericRepository<User>>();
+            var mockedUser = new Mock<User>();
+            mockedUsersRepo.Setup(x => x.GetAll(It.IsAny<Expression<Func<User, bool>>>()))
+                .Returns(new List<User>() { mockedUser.Object });
+
+            var service = new UsersService(mockedUnitOfWork.Object, mockedUsersRepo.Object);
+
+            var result =service.GetUserByUsername("the-super-cool-username-007");
+
+            Assert.AreEqual(mockedUser.Object, result);
+        }
+    }
+}
---
+++ CourseProject/CourseProject.Services.Tests/UsersServiceTests/RemoveAdFromSaved_Should.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using CourseProject.Data.UnitsOfWork;
+using CourseProject.Data.Repositories;
+using CourseProject.Models;
+
+namespace CourseProject.Services.Tests.UsersServiceTests
+{
+    [TestFixture]
+    public class RemoveAdFromSaved_Should
+    {
+        [Test]
+        public void RemovesTheAdWithCorrectIdFromUserSavedAds()
+        {
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedAdsRepo = new Mock<IGenericRepository<User>>();
+            var mockedUser = new Mock<User>();
+            var matchingAd = new Advertisement();
+            var mockedAd = new Mock<Advertisement>();
+
+            var adId = 55324;
+            matchingAd.Id = adId;
+            var data = new List<Advertisement> { matchingAd, mockedAd.Object };
+            mockedUser.Setup(x => x.SavedAds).Returns(data);
+
+            var service = new UsersService(mockedUnitOfWork.Object, mockedAdsRepo.Object);
+
+            service.RemoveAdFromSaved(adId, mockedUser.Object);
+
+            CollectionAssert.DoesNotContain(data, matchingAd);
+        }
+
+        [Test]
+        public void CallUnitOfWorkCommitMethod()
+        {
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedAdsRepo = new Mock<IGenericRepository<User>>();
+            var mockedUser = new Mock<User>();
+            mockedUser.Setup(x => x.SavedAds).Returns(new List<Advertisement>());
+            mockedUnitOfWork.Setup(x => x.Commit()).Verifiable();
+
+            var service = new UsersService(mockedUnitOfWork.Object, mockedAdsRepo.Object);
+
+            service.RemoveAdFromSaved(123, mockedUser.Object);
+
+            mockedUnitOfWork.Verify(x => x.Commit(), Times.Once);
+        }
+    }
+}
