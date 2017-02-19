f8969d984c66eb8a0438fbba5152275ea87463ec
Elena Zarkova <ezarkova@gmail.com>
2017-02-19 23:00:21 +0200

    Categories and cities services tests

---
+++ CourseProject/CourseProject.Services.Tests/CategoriesServiceTests/Constructor_Should.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CourseProject.Services.Tests.CategoriesServiceTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowArgumentNullException_WhenCitiesRepositoryIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CategoriesService(null));
+        }
+
+        [Test]
+        public void ThrowArgumentNullExceptionWithCorrectMessage_WhenCitiesRepositoryIsNull()
+        {
+            Assert.That(() => new CategoriesService(null),
+               Throws.ArgumentNullException.With.Message.Contains("Categories repository cannot be null."));
+        }
+    }
+}
---
+++ CourseProject/CourseProject.Services.Tests/CitiesServiceTests/Constructor_Should.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CourseProject.Services.Tests.CitiesServiceTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowArgumentNullException_WhenCitiesRepositoryIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CitiesService(null));
+        }
+
+        [Test]
+        public void ThrowArgumentNullExceptionWithCorrectMessage_WhenCitiesRepositoryIsNull()
+        {
+            Assert.That(() => new CitiesService(null),
+               Throws.ArgumentNullException.With.Message.Contains("Cities repository cannot be null."));
+        }
+    }
+}
